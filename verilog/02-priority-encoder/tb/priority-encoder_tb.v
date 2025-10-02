c630ae29d880791177d6f218ec10283847871a68
ebrarerturk <ebrarerturkk@hotmail.com>
2025-10-02 22:09:36 +0200

    added priority encoder testbench

--- verilog/02-priority-encoder/tb/priority-encoder_tb.v
+++ verilog/02-priority-encoder/tb/priority-encoder_tb.v
@@ -5,17 +5,17 @@ module test_priority_encoder;
     wire [1:0] out;
     wire valid;
 
+    // Instantiate the design
     priority_encoder dut(.in(in), .out(out), .valid(valid));
 
     initial begin
-        $dumpfile("wave.vcd");
-        $dumpvars(0, test_priority_encoder);
-
         $display("Priority Encoder Simulation");
+
         in = 4'b0000; #5 $display("in=%b -> out=%b valid=%b", in, out, valid);
         in = 4'b0001; #5 $display("in=%b -> out=%b valid=%b", in, out, valid);
         in = 4'b0011; #5 $display("in=%b -> out=%b valid=%b", in, out, valid);
         in = 4'b1111; #5 $display("in=%b -> out=%b valid=%b", in, out, valid);
+
         $finish;
     end
 endmodule
